d0a1ffa9ed489ce91f16f875c2f8a7e0595552ff
tommylee <tommylee@iasia.com.hk>
2021-05-20 14:38:18 +0800

    first commit

---
+++ ReplayUdpPcap/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReplayUdpPcap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args.FirstOrDefault() ?? throw new ArgumentException(".pcap file is not provided");
+
+            var rate = int.Parse(args.Skip(1).FirstOrDefault() ?? "10");
+            Console.WriteLine($"Send rate: {rate}p/s");
+
+            var cancellationSource = new CancellationTokenSource();
+            _ = SendAsync(file, rate, cancellationSource.Token);
+
+            Console.WriteLine("Please any key to stop...");
+            Console.ReadLine();
+
+            cancellationSource.Cancel();
+            Console.WriteLine("End");
+        }
+
+        static async Task SendAsync(string file, int rate, CancellationToken cancellation)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            
+            using var udpClient = new UdpClient();
+            var count = 0;
+
+            var captures = PcapFileReader.Read(file);
+
+            Console.WriteLine("Send started");
+
+            foreach (var (capture, ipPacket, udpPacket) in captures)
+            {
+                var address = ipPacket.DestinationAddress.ToString();
+
+                var requireTime = ++count * 1000 / rate;
+                var requirePause = requireTime - (int)stopwatch.Elapsed.TotalMilliseconds;
+                if (requirePause > 0)
+                {
+                    await Task.Delay(requirePause, cancellation);
+                }
+
+                cancellation.ThrowIfCancellationRequested();
+
+                if (count % 10 * rate == 0)
+                {
+                    Console.WriteLine($"sending {count}th packet...");
+                }
+
+                udpClient.Send(
+                    udpPacket.PayloadData, udpPacket.PayloadData.Length,
+                    address, udpPacket.DestinationPort);
+            }
+
+            Console.WriteLine("Send completed");
+        }
+    }
+}
